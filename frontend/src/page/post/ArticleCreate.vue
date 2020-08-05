1f451faa4f67b0d2537909bece643d0c06196350
ketkat001 <ketkat001@naver.com>
2020-08-05 13:52:50 +0900

    Quill Editor를 사용한 아티클생성 페이지

---
+++ frontend/src/page/post/ArticleCreate.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="main" class="main">
+    <div class="article-create container">
+      <h2>글 작성</h2>
+      <hr style="border: 1px solid rgb(196, 195, 208); margin-bottom: 30px;">
+      <quill-editor v-model="content"
+        ref="myQuillEditor"
+        :options="editorOption">
+      </quill-editor>
+      <b-button @click="createAction">작성완료</b-button>
+    </div>
+  </div>  
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        content: '<h2>I am Example</h2>',
+        editorOption: {
+        }
+      }
+    },
+    methods: {
+      createAction() {
+        var content2 = this.$refs.myQuillEditor.$options.propsData.value
+        console.log(content2)
+      }
+    },
+    
+  }
+</script>
+
+<style>
+  .article-create {
+    margin-top: 120px;
+  }
+</style>
