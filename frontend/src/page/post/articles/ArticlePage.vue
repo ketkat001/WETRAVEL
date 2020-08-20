65cc27209b28b6cb541c87a2d360b3b71f074f37
ketkat001 <ketkat001@naver.com>
2020-08-20 16:18:44 +0900

    좋아요 아이콘 삭제

--- frontend/src/page/post/articles/ArticlePage.vue
+++ frontend/src/page/post/articles/ArticlePage.vue
@@ -57,7 +57,6 @@
         <div class="article-comment">
           <div class="comments-header">
             <div class="comments-stats">
-              <span class="m-3"><font-awesome-icon icon="thumbs-up" size="lg" class="mx-3"></font-awesome-icon>1</span>
               <span class="m-3"><font-awesome-icon icon="comment" size="lg" class="mx-3"></font-awesome-icon>{{numOfComments}}</span>
             </div>
           </div>
@@ -75,7 +74,7 @@
 
 <script>
 import axios from 'axios'
-import { faThumbsUp as thumbsUp_SolidIcon, faComment } from "@fortawesome/free-solid-svg-icons";
+import { faComment } from "@fortawesome/free-solid-svg-icons";
 import comments from '../comments/Comments.vue'
 let lats = ''
 let longs = ''
